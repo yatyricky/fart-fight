6cb0969dcc78ff3e3fe5e316f2626c0e971e74d7
Rick Sun <yatyricky@gmail.com>
2018-02-05 18:19:22 +0800

    - project clean-up - added linode server

--- ClientUnity/Assets/Scripts/Configs.cs
+++ ClientUnity/Assets/Scripts/Configs.cs
@@ -1,7 +1,8 @@
 ﻿public class Configs
 {
     //public static string SERVER_URL = "ws://127.0.0.1:11337";
-    public static string SERVER_URL = "ws://118.126.90.81:11337";
+    //public static string SERVER_URL = "ws://118.126.90.81:11337";
+    public static string SERVER_URL = "ws://74.207.250.141:11337";
     public static int NUKE_POWER = 5;
     public static float INTERVAL = 5f;
 }
