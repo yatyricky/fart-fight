4dc3bfec8349a9654434da79f65402b3d039be86
Rick Sun <yatyricky@gmail.com>
2018-02-07 16:52:07 +0800

    ux improve - default hide room number field

---
+++ ClientUnity/Assets/Scripts/Behaviours/CheckAutoMatch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class CheckAutoMatch : MonoBehaviour
+{
+    public GameObject InputRoomIdObject;
+    public InputField InputRoomIdField;
+    public GameObject CheckTickObject;
+
+    private bool isChecked;
+
+    private void Awake()
+    {
+        isChecked = true;
+    }
+
+    public void OnClickCheck()
+    {
+        if (isChecked)
+        {
+            isChecked = false;
+            CheckTickObject.SetActive(false);
+            InputRoomIdObject.SetActive(true);
+            gameObject.transform.DOMoveY(-258f, 0.5f).SetEase(Ease.OutCubic);
+        }
+        else
+        {
+            isChecked = true;
+            CheckTickObject.SetActive(true);
+            InputRoomIdObject.SetActive(false);
+            InputRoomIdField.text = "";
+            gameObject.transform.DOMoveY(-130f, 0.5f).SetEase(Ease.OutCubic);
+        }
+    }
+
+}
