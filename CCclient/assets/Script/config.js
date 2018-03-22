955d6f4696d2b7ad205d855c0cd1393cfe2d03a7
Rick Sun <yatyricky@gmail.com>
2018-03-22 15:54:48 +0800

    update server

--- CCclient/assets/Script/config.js
+++ CCclient/assets/Script/config.js
@@ -1,4 +1,4 @@
-window.serverURL = "ws://74.207.250.141:11337";
+window.serverURL = "https://g.nefti.me:11337";
 // window.serverURL = "ws://118.126.90.81:3000";
 window.INTERVAL = 5;
 window.TIME_OUT = 5;
